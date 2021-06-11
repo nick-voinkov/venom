9047ea22c6d22aae9d64e0f8d81fc9440c67ef00
nick-voinkov <85644944+nick-voinkov@users.noreply.github.com>
2021-06-12 01:28:26 +0600

    Написали правила адаптивности для веб-сайта, в конце добавили приятную анимацию

--- js/main.js
+++ js/main.js
@@ -1,2 +1,15 @@
 $(document).ready(function() {
+  const headerClosed = document.querySelector('.header__closed');
+  const headerList = document.querySelector('.header__list');
+  const headerOpen = document.querySelector('.header__open');
+
+  headerClosed.addEventListener('click', () => {
+    headerList.classList.add('open');
+    headerOpen.classList.add('open');
+  });
+
+  headerOpen.addEventListener('click', () => {
+    headerList.classList.remove('open');
+    headerOpen.classList.remove('open');
+  });
 });
